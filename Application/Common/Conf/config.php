d49e1db5db15009b3418aa15453c85d24cc3de25
869616032 <15132041467@163.com>
2017-04-02 18:26:45 +0800

    第一次提交

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'URL_MODEL' => 2,
+    //查看文件运行时间各种资料
+//    'SHOW_PAGE_TRACE' => true,
+
+);
---
+++ Application/Home/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'bdm272483561.my3w.com',
+    'DB_NAME' => 'bdm272483561_db',
+    'DB_USER' => 'bdm272483561',
+    'DB_PWD' => 'lf869616032',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'cb_',
+);
